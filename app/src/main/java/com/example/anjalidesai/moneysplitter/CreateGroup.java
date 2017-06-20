9500c91420c44bba0504dd1aa72f3b676103acda
UltimateAnjali <anjali21295@gmail.com>
2017-06-20 19:59:33 -0400

    come changes to code

--- app/src/main/java/com/example/anjalidesai/moneysplitter/CreateGroup.java
+++ app/src/main/java/com/example/anjalidesai/moneysplitter/CreateGroup.java
@@ -1,146 +1,45 @@
 package com.example.anjalidesai.moneysplitter;
 
+import java.util.ArrayList;
+import java.util.List;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
-import android.text.TextUtils;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.AdapterView;
+import android.app.Activity;
+import android.database.Cursor;
+import android.util.Log;
+import android.view.Menu;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.SimpleAdapter;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+public class CreateGroup extends Activity{
 
-import static android.Manifest.permission.READ_CONTACTS;
-
-public class CreateGroup extends AppCompatActivity{
-
-    private static final int REQUEST_READ_CONTACTS = 0;
-    AutoCompleteTextView mMemberName = null;
-    private ArrayList<Map<String, String>> arrayListMap;
-    private SimpleAdapter simpleAdapter;
+    private AutoCompleteTextView mMemberName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_group);
 
-        mMemberName = (AutoCompleteTextView)findViewById(R.id.memberATV);
-        arrayListMap = new ArrayList<Map<String, String>>();
-        simpleAdapter = new SimpleAdapter(this, arrayListMap, R.layout.cust_contact_view,
-                new String[] { "Name", "Phone", "Type" }, new int[] {
-                R.id.ccontName, R.id.ccontNo, R.id.ccontType });
-        mMemberName.setAdapter(simpleAdapter);
-
-        populatePeopleData();
-        PopulatePeopleList();
-
-        mMemberName.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> av, View arg1, int index,
-                                    long arg3) {
-                Map<String, String> map = (Map<String, String>) av.getItemAtPosition(index);
-                String name  = map.get("Name");
-                String number = map.get("Phone");
-                mMemberName.setText(""+name+"<"+number+">");
-            }
-        });
+        mMemberName = (AutoCompleteTextView) findViewById(R.id.memberATV);
+        mMemberName.setAdapter(new ArrayAdapter<String>(getApplicationContext(),
+                R.layout.single_contact,R.id.tv_ContactName,getAllContactNames()));
     }
 
-    private void populatePeopleData() {
-        if (!mayRequestContacts()) {
-            return;
-        }
-    }
+    private List<String> getAllContactNames(){
+        List<String> lContactNamesList = new ArrayList<String>();
+        try{
+            Cursor lPeople = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
 
-    private boolean mayRequestContacts() {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            return true;
-        }
-        if (checkSelfPermission(READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
-            return true;
-        }
-        if (shouldShowRequestPermissionRationale(READ_CONTACTS)) {
-            Toast.makeText(getApplicationContext(),"Not permitted",Toast.LENGTH_SHORT).show();
-        } else {
-            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
-        }
-        return false;
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == REQUEST_READ_CONTACTS) {
-            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                populatePeopleData();
-            }
-        }
-    }
-
-    public  void PopulatePeopleList(){
-        arrayListMap.clear();
-        // Check the SDK version and whether the permission is already granted or not.
-
-        Cursor people = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
-        while (people.moveToNext()) {
-            String contactName = people.getString(people.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-            String contactId = people.getString(people.getColumnIndex(ContactsContract.Contacts._ID));
-            String hasPhone = people.getString(people.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
-
-            if ((Integer.parseInt(hasPhone) > 0)) {
-                // You know have the number so now query it like this
-                Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
-                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + contactId, null, null);
-                while (phones.moveToNext()) {
-                    //store numbers and display a dialog letting the user select which.
-                    String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                    String numberType = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.TYPE));
-                    Map<String, String> NamePhoneType = new HashMap<String, String>();
-                    NamePhoneType.put("Name", contactName);
-                    NamePhoneType.put("Phone", phoneNumber);
-                    if (numberType.equals("0"))
-                        NamePhoneType.put("Type", "Work");
-                    else if (numberType.equals("1"))
-                        NamePhoneType.put("Type", "Home");
-                    else if (numberType.equals("2"))
-                        NamePhoneType.put("Type", "Mobile");
-                    else
-                        NamePhoneType.put("Type", "Other");
-                    //Then add this map to the list.
-                    arrayListMap.add(NamePhoneType);
+            if(lPeople != null){
+                while (lPeople.moveToNext()){
+                    lContactNamesList.add(lPeople.getString(lPeople.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)));
                 }
-                phones.close();
             }
+        }catch (NullPointerException e){
+            Log.e("getAllContactNames()",e.getMessage());
         }
-        people.close();
-
+        return lContactNamesList;
     }
 }
 
