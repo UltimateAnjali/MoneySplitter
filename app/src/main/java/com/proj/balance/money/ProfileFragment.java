833d1606b1af0b18583416d755c69d4c0f70d4f3
UltimateAnjali <anjali21295@gmail.com>
2017-10-25 20:47:40 -0400

    bottom navigation added

--- app/src/main/java/com/proj/balance/money/ProfileFragment.java
+++ app/src/main/java/com/proj/balance/money/ProfileFragment.java
@@ -1,12 +1,24 @@
 package com.proj.balance.money;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.squareup.picasso.Picasso;
 
 public class ProfileFragment extends Fragment {
+
+    ImageView imageView;
+    TextView username;
+    EditText useremail;
 
     public ProfileFragment() {
     }
@@ -22,9 +34,27 @@ public class ProfileFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        imageView = (ImageView)view.findViewById(R.id.profile_image);
+        username = (TextView)view.findViewById(R.id.user_name);
+        useremail = (EditText) view.findViewById(R.id.user_email);
+
+        username.setText(UserData.userGivenName+" "+UserData.userFamilyName);
+        useremail.setText(UserData.userEmail);
+
+        /*Glide.with(getContext()).load(Uri.parse(UserData.userPhoto))
+                .thumbnail(0.5f)
+                .crossFade()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(imageView);*/
+
+        Picasso.with(getContext())
+                .load(Uri.parse(UserData.userPhoto))
+                .noFade()
+                .transform(new CircleTransform())
+                .into(imageView);
+        return view;
     }
 
 }
